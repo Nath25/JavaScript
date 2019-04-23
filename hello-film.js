5ace7e159e75a1da6863cc713d74ad28e8f7fb73
linux <47882055+Nath25@users.noreply.github.com>
2019-04-23 17:55:03 +0200

    Create hello-film.js

---
+++ hello-film.js
@@ -0,0 +1,8 @@
+// Define some variables
+const filmName = 'Au Revoir Là-haut';
+const filmDirector = 'Albert Dupontel';
+const filmYear = 2017;
+const message = 'My favorite film is ' + filmName + ' directed by  ' + filmDirector + ' in ' + filmYear ;
+
+// Display a popup
+alert(message);
